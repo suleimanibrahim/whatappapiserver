b8d1f3b83f303e720c24377584d099fa0b251674
suleiman ibrahim inuwa <isuleimanibrahim@gmail.com>
2024-05-08 09:13:25 -0700

    first commit

---
+++ src/main/java/com/whatapp/whatappapiserver/WhatAppApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.whatapp.whatappapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatAppApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatAppApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whatapp/whatappapiserver/WhatAppApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatapp.whatappapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatAppApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
